227e4933935e81654077d71645c683f49d1da0f5
dwwhite-designs <69012675+dwwhite-designs@users.noreply.github.com>
2021-09-04 10:32:58 -0700

    Add files via upload
    Uploaded 1st set of test files to run on the nvidia jetson

---
+++ MoveMotor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//#include <JetsonGPIO>
+//using namespace GPIO; // optional
+
+
+int MoveMotor(int power, int gpio)
+{
+    //std::cin.clear(); // reset any error flags
+    //std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
+    //std::cin.get(); // get one more char from the user
+    return 0;
+}
---
+++ WinstonTestCode.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//#include <JetsonGPIO>
+//using namespace GPIO; // optional
+
+
+int main()
+{
+
+    std::cout << "Hello World!";
+
+    std::cin.clear(); // reset any error flags
+    //std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
+    //std::cin.get(); // get one more char from the user
+    return 0;
+}
