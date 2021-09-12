fa9d2234b60c428555eee6a0d245e7f8835ec3c7
Dave <dwwhite.designs@gmail.com>
2021-09-12 16:17:44 -0700

    Adding new MoveMotor method

--- WinstonTestCode.cpp
+++ WinstonTestCode.cpp
@@ -6,10 +6,17 @@
 int main()
 {
 
-    std::cout << "Hello World!";
+	std::cout << "Hello World!\r";
 
-    std::cin.clear(); // reset any error flags
+	std::cin.clear(); // reset any error flags
     //std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
     //std::cin.get(); // get one more char from the user
     return 0;
+}
+
+
+int MoveMotor(int motor)
+{
+
+	return 0;
 }
