4d7c3bf96757fb1316d3824c18679b056d0528a2
dwwhite-designs <69012675+dwwhite-designs@users.noreply.github.com>
2023-05-07 11:36:19 -0500

    Update ArduinoControl.py
    added exit paramater

--- ArduinoControl.py
+++ ArduinoControl.py
@@ -6,16 +6,9 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
 
+x = True
 
-   """  while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(line)
-        ser.write("Hello from Raspberry Pi!\n".encode('utf-8'))
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-        time.sleep(1) """
-    
+
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
@@ -23,6 +16,9 @@ if __name__ == '__main__':
     
         print("Enter a, s, d, or f")
         command = input()
+        if command == "f"
+            x = False
+            
         ser.write(command + "/n".encode('utf-8'))
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
