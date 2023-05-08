c7f2ed7b066a1867936575094a7aedfabdc55fcf
dwwhite-designs <69012675+dwwhite-designs@users.noreply.github.com>
2023-05-07 20:33:44 -0500

    clean up

--- ArduinoControl.py
+++ ArduinoControl.py
@@ -14,23 +14,6 @@ def write_read(inputs):
 
 
 
-""" # Call the 'bluetoothctl' command to retrieve a list of connected devices
-process = subprocess.Popen(['bluetoothctl', 'paired-devices'], stdout=subprocess.PIPE)
-
-# Extract the MAC addresses of the connected devices
-output, error = process.communicate()
-connected_devices = []
-for line in output.decode().split('\n'):
-    if 'Device' in line:
-        parts = line.split()
-        mac_address = parts[1]
-        connected_devices.append(mac_address)
-
-# Print the MAC addresses of the connected devices
-for address in connected_devices:
-    print(f"Connected device with MAC address: {address}") """
-
-
 try:
     # Call the 'bluetoothctl' command to retrieve a list of connected devices
     process = subprocess.Popen(['bluetoothctl', 'paired-devices'], stdout=subprocess.PIPE)
